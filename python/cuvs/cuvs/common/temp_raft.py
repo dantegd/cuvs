b1404c53c5dd4f611cf9a9193ac40a74cac0b7d6
Dante Gama Dessavre <danteg@nvidia.com>
2024-02-22 21:44:15 -0600

    FIX cython fixes

--- python/cuvs/cuvs/common/temp_raft.py
+++ python/cuvs/cuvs/common/temp_raft.py
@@ -18,6 +18,11 @@
 
 # This file has code that will be upstreamed to RAFT
 
+import functools
+
+from pylibraft.common import DeviceResources
+
+
 def auto_sync_resources(f):
     """
     This is identical to auto_sync_handle except for the proposed name change.

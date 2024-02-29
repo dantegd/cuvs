8b4fd67c679fe40992d839e0e9ddda169363303f
Dante Gama Dessavre <danteg@nvidia.com>
2024-02-29 00:15:34 -0600

    FIX cython fixes

--- python/cuvs/cuvs/neighbors/cagra/cagra.pyx
+++ python/cuvs/cuvs/neighbors/cagra/cagra.pyx
@@ -199,7 +199,7 @@ def build_index(IndexParams index_params, dataset, resources=None):
     cdef cuvsResources_t res_
     cdef cuvsError_t cstat
 
-    print(resources is None)
+    print(not resources or resources is None)
 
     # if resources is None:
     cstat = cuvsResourcesCreate(&res_)

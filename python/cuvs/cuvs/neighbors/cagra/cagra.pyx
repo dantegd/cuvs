ba43a8947e45159c3f5134557490a1a0f3d722eb
Dante Gama Dessavre <danteg@nvidia.com>
2024-02-28 22:36:02 -0600

    FIX cython fixes

--- python/cuvs/cuvs/neighbors/cagra/cagra.pyx
+++ python/cuvs/cuvs/neighbors/cagra/cagra.pyx
@@ -219,7 +219,7 @@ def build_index(IndexParams index_params, dataset, resources=None):
     with cuda_interruptible():
         print("AE")
         build_status = cuvsCagraBuild(
-            resources_,
+            resources_[0],
             params,
             &dataset_dlpack,
             idx.index
